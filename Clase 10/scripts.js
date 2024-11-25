e2147b5d8816c6e34e4fd560cfe00e98a1ddd569
Norberto Llopiz <norberto.llopiz@bue.edu.ar>
2024-11-25 17:26:38 -0300

    pre clase 11

---
+++ Clase 10/scripts.js
@@ -0,0 +1,25 @@
+
+
+/* Tipos de datos */
+//String = cadena de caracteres
+console.log("Hola mundo 12345 $%& . , ")
+//Números 
+console.log(45)
+console.log(45.45678)
+//Booleanos  Boole
+//Verdader o Falso
+console.log(true)
+console.log(false)
+
+console.log(4587*4568-1245/78)
+
+console.log("Ahora en la clase somos: ", 340/2, "Yupiiiiiiiii")
+
+let nombreCliente = "María" //operador de asignación
+var apellido //inicializar una variable
+apellido = "Pérez"
+
+
+ 
+
+console.log(nombreCliente, apellido)
