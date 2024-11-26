76f4141c5b4de6c452a8f7d79087df3d25240cae
Norberto Llopiz <norberto.llopiz@bue.edu.ar>
2024-11-26 19:41:20 -0300

    ;

--- Clase 11/scripts.js
+++ Clase 11/scripts.js
@@ -1,35 +1,36 @@
-console.log("OK")
+console.log("OK");
 
-const nombreUsuario = document.getElementById("nombre").value
+const nombreUsuario = document.getElementById("nombre").value;
 
-console.log(nombreUsuario)
+console.log(nombreUsuario);
 
-const edadUsuario = document.getElementById("edad").value
-console.log(edadUsuario)
+const edadUsuario = document.getElementById("edad").value;
+console.log(edadUsuario);
 
-const parrafoSaludo = document.getElementById("saludo")
-//console.log(parrafoSaludo)
+const parrafoSaludo = document.getElementById("saludo");
+//console.log(parrafoSaludo);
 
-parrafoSaludo.innerText = "Hola " + nombreUsuario
+parrafoSaludo.innerText = "Hola " + nombreUsuario;
 
-const mayorDeEdad = document.getElementById("mayorDeEdad")
-//console.log(mayorDeEdad)
-mayorDeEdad.innerText = "Tenés " + edadUsuario + " años"
+const mayorDeEdad = document.getElementById("mayorDeEdad");
+//console.log(mayorDeEdad);
+mayorDeEdad.innerText = "Tenés " + edadUsuario + " años";
 
-console.log(edadUsuario >= 18) // a + b
+console.log(edadUsuario >= 18); // a + b
 
-const esMayor = document.getElementById("esMayor")
+const esMayor = document.getElementById("esMayor");
 
-//esMayor.innerText = "Sos mayor de edad?: " + (edadUsuario >= 18)
+//esMayor.innerText = "Sos mayor de edad?: " + (edadUsuario >= 18);
 
 if (edadUsuario > 18 && edadUsuario < 41) {
-    esMayor.innerText = "Tiene un 20% de descuento en Vinos"
+    esMayor.innerText = "Tiene un 20% de descuento en Vinos";
 }
 else if (edadUsuario > 40 && edadUsuario < 61) { //AND
-    esMayor.innerText = "Tiene un 40% de descuento en Vinos"
+    esMayor.innerText = "Tiene un 40% de descuento en Vinos";
 } 
 else {
-    esMayor.innerText = "Sólo podés comprar agua y gaseosas"
+    esMayor.innerText = "Sólo podés comprar agua y gaseosas";
 }
 
 // voy a tu casa en auto || en micro OR
+
